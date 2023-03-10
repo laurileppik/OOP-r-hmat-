4bc33da395c9ace2f734ffb2891ce5539353b2c6
laurileppik <laurile02@gmail.com>
2023-03-10 17:32:12 +0200

    Lisasin mõne funktisonaalsuse ning võimaluse näha mis asjad ruumis on.

--- Mäng.java
+++ Mäng.java
@@ -1,5 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+import java.util.ArrayList;
 
 public class Mäng {
     private Ruum[] ruumid;
@@ -9,40 +11,102 @@ public class Mäng {
     }
 
     public Mäng() {
-        ruumid = new Ruum[3];
-        ruumid[0] = new Ruum("Koobas", "Oled pimedas koopas.");
-        ruumid[1] = new Ruum("Mets", "Oled metsas.");
-        ruumid[2] = new Ruum("Rannik", "Oled rannikul.");
+        Asi oda = new Asi("oda", "Terava otsaga pead torkama.");
+        Asi ori = new Asi("ori", "Saad enda eest tööle panna.");
+        Asi tõrvik = new Asi("tõrvik", "Ära ennast põlema pane.");
+
+        ArrayList<Asi> asjad=new ArrayList<>();
+
+        ruumid = new Ruum[6];
+        ruumid[0] = new Ruum("Koobas", "Oled pimedas koopas.",asjad);
+        asjad.add(oda);
+        asjad.add(tõrvik);
+        ruumid[1] = new Ruum("Koopasuu", "Näed koopasuud.",asjad);
+        ruumid[2] = new Ruum("Kaljuäär", "Näed kaljuäärt.",asjad);
+        ruumid[3] = new Ruum("Kalju", "Oled kalju ääre peal.",asjad);
+
+        ruumid[4] = new Ruum("Kuristik", "Hüppasid kuristikku.",asjad);
+        ruumid[5] = new Ruum("Kaljuäär", "Liigud mööda kaljuäärt",asjad);
     }
 
     public static void main(String[] args) {
         Mäng mäng = new Mäng();
         Mängija mängija = new Mängija(mäng.getRuumid()[0]);
 
-        Asi oda = new Asi("oda", "Terava otsaga pead torkama.");
-        Asi ori = new Asi("ori", "Saad enda eest tööle panna.");
-        Asi tõrvik=new Asi("tõrvik","Ära ennast põlema pane.");
-
         /**mängija.võtaAsi(oda);
-        mängija.võtaAsi(ori);
-        mängija.vaataSeljakotti();**/
+         mängija.võtaAsi(ori);
+         mängija.vaataSeljakotti();**/
 
         Scanner in = new Scanner(System.in);
         Random rand = new Random();
 
         System.out.println("Tere tulemast!");
         System.out.println();
-
+        System.out.println(mängija.getAsukoht().getRuumiKirjeldus());
         boolean mängKäib = true;
+        int hetkeneRuum = 0;
+        label:
         while (mängKäib) {
-            System.out.println(mängija.getAsukoht().getRuumiKirjeldus());
-            String input=in.nextLine();
-            if (input.equals("a"))
-                break;
-            else if (input.equals("l")) {
-                mängija.liigu(mäng.getRuumid()[1]);
-            } else if (input.equals("t")) {
-                mängija.liigu(mäng.getRuumid()[0]);
+            String input = in.nextLine();
+            switch (input) {
+                case "a":
+                    break label;
+                case "l":
+                    try {
+                        mängija.liigu(mäng.getRuumid()[hetkeneRuum + 1]);
+                        hetkeneRuum++;
+                        System.out.println(mäng.getRuumid()[hetkeneRuum].toString());
+                    } catch (Exception e) {
+                        System.out.println("Sein on ees.");
+                    }
+                    if (hetkeneRuum == 3) {
+                        System.out.println("Sul on 2 valikut: Saad minna mööda kaljuäärt edasi või proovida ennast alla libistada.");
+                        hetkeneRuum++;
+                    }
+                    break;
+                case "t":
+                    if (hetkeneRuum == 5) {
+                        hetkeneRuum--;
+                    }
+                    try {
+                        mängija.liigu(mäng.getRuumid()[hetkeneRuum - 1]);
+                        hetkeneRuum--;
+                        System.out.println(mäng.getRuumid()[hetkeneRuum].toString());
+                    } catch (Exception e) {
+                        System.out.println("Sein on ees.");
+                    }
+                    if (hetkeneRuum == 3) {
+                        System.out.println("Sul on 2 valikut: Saad minna mööda kaljuäärt edasi või proovida ennast alla libistada.");
+                    }
+
+                    break;
+                case "v":
+                    mängija.vaataSeljakotti();
+                    break;
+                case "libista":
+                    System.out.println("...");
+                    try {
+                        TimeUnit.SECONDS.sleep(2);
+                        System.out.println("Kukud");
+                        TimeUnit.SECONDS.sleep(2);
+                        System.out.println("Ikka kukud");
+                        TimeUnit.SECONDS.sleep(2);
+                    } catch (Exception e) {
+                        System.out.println("Nüüd on hilja ennast päästa.");
+                    }
+                    System.out.println("Maa on päris valus.");
+                    System.out.println("Mäng läbi, sa kaotasid.");
+                    mängKäib = false;
+                    //Peaks võtma mingi asja ja inventorisse lisama.
+                    break;
+                case "võta":
+                    System.out.println(mäng.getRuumid()[hetkeneRuum].toString());
+                    //mängija.võtaAsi();
+                    //Peaks soutima asja kirjelduse. NT oda puhul Terava otsaga pead torkama.
+                    break;
+                case "vaata":
+                    System.out.println();
+                    break;
             }
         }
     }
--- Ruum.java
+++ Ruum.java
@@ -1,10 +1,18 @@
+import java.util.ArrayList;
+
 public class Ruum {
     private String nimi;
     private String ruumiKirjeldus;
+    private ArrayList<Asi> asjadRuumis;
 
-    public Ruum(String nimi, String ruumiKirjeldus) {
+    public Ruum(String nimi, String ruumiKirjeldus,ArrayList<Asi> asjadRuumis) {
         this.nimi = nimi;
         this.ruumiKirjeldus = ruumiKirjeldus;
+        this.asjadRuumis=asjadRuumis;
+    }
+
+    public ArrayList<Asi> getAsjadRuumis() {
+        return asjadRuumis;
     }
 
     public String getNimi() {
@@ -21,5 +29,11 @@ public class Ruum {
 
     public void setRuumiKirjeldus(String ruumiKirjeldus) {
         this.ruumiKirjeldus = ruumiKirjeldus;
+    }
+
+    public String toString() {
+        if (!asjadRuumis.isEmpty())
+            return "Ruumis " + nimi + " on järgnevad asjad: " +asjadRuumis;
+        return "Ruumis ei ole midagi mida sa kasutada saaksid.";
     }
 }
